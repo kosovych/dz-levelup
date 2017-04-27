a15880309dbbe72fb18269afb2a866e2bb8aa6ad
kosovych <bassicinbox@gmail.com>
2017-04-27 16:50:26 +0300

    add 21-game

---
+++ 20-04-2017/21/main.js
@@ -0,0 +1,41 @@
+
+let YorN = confirm("Приветствуем вас в игре \"21\". \
+	Вам выпал шанс сыграть первым. \
+	Нажмите \"OK\" для начала или \"Отмена\" для выхода.");
+
+
+
+if (YorN) {
+	var user1 = randomNumb();
+} else alert("Вы отменили участие в игре.");
+
+function randomNumb() {
+	var contin = true;
+
+	alert("Сейчас для вас сгенерируется случайное число.\
+		Нажмите \"ОК\" чтобы проложить.");
+	var userNumb = 0;
+	
+	for (;;){
+		
+		userNumb += Math.round(Math.random()*11);
+		if (userNumb > 21) break;
+		contin = confirm(`Ваше число ${userNumb}. Если хотите продолжить - нажмите \"ОК\",\
+			 чтобы закончить и/или передать ход нажмите \"Отмена\"`);
+		if (contin === false) break;
+
+	};
+	return userNumb;
+};
+
+if (user1 > 21) {
+	console.log(`Вы проиграли. Ваше число больше ${user1} и оно больше 21`);
+} else {
+	var user2 = randomNumb();
+};
+
+if (user1 > user2) {
+	console.log("Первый игрок выиграл. Поздравляем!");
+} else if (user1 < user2) {
+	console.log("Второй игрок выиграл. Поздравляем!");
+} else console.log("Победила дружба! :)");
